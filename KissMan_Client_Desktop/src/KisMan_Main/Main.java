c5efc7b98140eb02d1e16f05ea5580d460981554
yagi815@gmail.com <yagi815@gmail.com@aa83d6d0-1cf3-d709-f7b5-24625933a30a>
2011-03-09 07:49:32 +0000

    First Import

---
+++ KissMan_Client_Desktop/src/KisMan_Main/Main.java
@@ -0,0 +1,14 @@
+package KisMan_Main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
